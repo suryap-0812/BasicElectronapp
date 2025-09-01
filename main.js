851c7be4c71b25ab771f142a77618da2b5b80aef
Surya <surya.pmain2005@gmail.com>
2025-09-01 11:16:04 +0530

    second commit

---
+++ main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+// Run when app is ready
+app.whenReady().then(() => {
+  createWindow();
+
+  // 🔑 Check for updates
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
---
+++ renderer.js
@@ -0,0 +1,5 @@
+const button = document.getElementById('btn');
+button.addEventListener('click', () => {
+  alert('Button clicked!');
+});
+console.log('Renderer process loaded');
